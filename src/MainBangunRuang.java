c9b4ad82d3ff3a71e11f97559e6585304a1677dc
Nazheef Thareq <nazheefthareq15@gmail.com>
2025-03-06 20:49:03 +0700

    add: block array loop and average volume

--- src/MainBangunRuang.java
+++ src/MainBangunRuang.java
@@ -25,5 +25,29 @@ public class MainBangunRuang {
         double avgVolumeKubus = totalVolumeKubus / cubesArray.length;
         System.out.println("Rata-rata Volume Kubus adalah: "+ avgVolumeKubus + "\n");
 
+        System.out.println("========================== BALOK ============================");
+        Balok[] blockArray;
+        blockArray = new Balok[5];
+
+        blockArray[0] = new Balok(4, 3, 3);
+        blockArray[1] = new Balok(5, 2, 3);
+        blockArray[2] = new Balok(3, 2, 4);
+        blockArray[3] = new Balok(5, 3, 2);
+        blockArray[4] = new Balok(4, 3, 2);
+
+        index = 0;
+        for (Balok loopBalok : blockArray) {
+            System.out.println("Volume Balok ke-"+ (index+1)+ " adalah:"+ loopBalok.ComputeAndSetVolume());
+            index++;
+        }
+
+        double totalVolumeBalok = 0;
+        for (Balok loopBalok : blockArray) {
+            totalVolumeBalok += loopBalok.ComputeAndSetVolume();
+        }
+
+        double avgVolumeBalok = totalVolumeBalok / blockArray.length;
+        System.out.println("Rata-rata Volume Balok adalah: "+ avgVolumeBalok + "\n");
+
     }
 }
